329a6ba087269cd0b29af7780eb4a486a8f4bf6e
HanzlaAnjum012 <126371530+HanzlaAnjum012@users.noreply.github.com>
2023-02-26 09:36:15 +0500

    Update settings.py

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "Hanzla@27051999"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','hanzlaanjum.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','hanzlaanjum.findout.pk']
 
 
 # Application definition
@@ -79,14 +77,14 @@ WSGI_APPLICATION = 'MyWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hanzla$default',
+        'HOST':'hanzla.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
+        'USER':'hanzla',
+        'PASSWORD':'Hanzla@27051999',
     }
 }
-
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
